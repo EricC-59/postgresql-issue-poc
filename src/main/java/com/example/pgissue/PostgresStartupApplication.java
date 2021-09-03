77975f5fd6a40cffe9baeab19d76d965d25bb7fc
EricC-59 <invalid@example.com>
2021-09-03 11:52:55 +0200

    Initial commit

---
+++ src/main/java/com/example/pgissue/PostgresStartupApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresStartupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pgissue/PostgresStartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresStartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
